59f75cff55b687d4da0cd5d4e6247a062d526c60
Zane Deso <zanemilo@yahoo.com>
2024-07-22 13:56:36 -0400

    add functionality for simple collision testing. Create collision box for testing, updating its color if the img goes over the same area as teh collision area.

--- main.py
+++ main.py
@@ -17,11 +17,21 @@ class Game:
         self.img_pos = [160, 260]
         self.movement = [False, False]
 
+        self.collision_area = pygame.Rect(50, 50, 300, 50) # create area to test collision with
+
+
     def run(self):
         while True:
             self.screen.fill((14, 219, 248))  # reset screen with background color (R, B, G)
             self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5  # y value += 1 if upkey is down, -1 if downkey is down, 0 otherwise. speed multiplier is last int
             self.screen.blit(self.img, self.img_pos)  # draw/blit img onto screen
+
+            img_r = pygame.Rect(self.img_pos[0], self.img_pos[1], self.img.get_width(), self.img.get_height())  # render collision box for img obj, update its pos based on pos of img, update its size based on img size
+            if img_r.colliderect(self.collision_area): # if the collision box of img (img_r) overlaps collision area
+                pygame.draw.rect(self.screen, (0, 100, 255), self.collision_area) # update color to light blue
+            else:
+                pygame.draw.rect(self.screen, (0, 90, 155), self.collision_area) # otherwise update to color to darker color
+
             
             for event in pygame.event.get():  # handles all kinds of events, including key press, mouse movement etc.
                 if event.type == pygame.QUIT:
