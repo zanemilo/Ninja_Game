fb0c9e2511661094ab53d3e4555d679a5fcef06a
Zane Deso <zanemilo@yahoo.com>
2024-07-22 13:59:26 -0400

    refactor code rendering order so cloud is over collision area

--- main.py
+++ main.py
@@ -22,9 +22,9 @@ class Game:
 
     def run(self):
         while True:
+
+            
             self.screen.fill((14, 219, 248))  # reset screen with background color (R, B, G)
-            self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5  # y value += 1 if upkey is down, -1 if downkey is down, 0 otherwise. speed multiplier is last int
-            self.screen.blit(self.img, self.img_pos)  # draw/blit img onto screen
 
             img_r = pygame.Rect(self.img_pos[0], self.img_pos[1], self.img.get_width(), self.img.get_height())  # render collision box for img obj, update its pos based on pos of img, update its size based on img size
             if img_r.colliderect(self.collision_area): # if the collision box of img (img_r) overlaps collision area
@@ -32,6 +32,8 @@ class Game:
             else:
                 pygame.draw.rect(self.screen, (0, 90, 155), self.collision_area) # otherwise update to color to darker color
 
+            self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5  # y value += 1 if upkey is down, -1 if downkey is down, 0 otherwise. speed multiplier is last int
+            self.screen.blit(self.img, self.img_pos)  # draw/blit img onto screen
             
             for event in pygame.event.get():  # handles all kinds of events, including key press, mouse movement etc.
                 if event.type == pygame.QUIT:
