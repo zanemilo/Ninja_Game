077a3006431e25dff30390a39021815abfe84ec0
Zane Deso <zanemilo@yahoo.com>
2024-07-23 12:05:08 -0400

    syntax error fix in Tilemap class

--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -4,5 +4,6 @@ class Tilemap:
         self.tilemap = {}  # Every tile is on a square grid
         self.offgrid_tiles = []  # Tiles for off grid, all over the place
 
-        
+        for i in range(10):
+            self.tilemap[str(3 + i) + ';10'] = {'type': 'grass', 'variant': 1, 'pos': (3 + i, 10)}
         
