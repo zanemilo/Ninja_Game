9e4e03d5fd2fc79d0dafb9a94cbf66f0df999437
Zane Deso <zanemilo@yahoo.com>
2024-07-23 12:13:10 -0400

    added a vertical stone wall to for loop iteration

--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -7,6 +7,7 @@ class Tilemap:
         # Concatonates location based on for loop i with fixed position of 10 as denoted by ';10', 
         # this location contains a dict that holds informaiton about the tile such as type, variant, and position.
         for i in range(10):
-            self.tilemap[str(3 + i) + ';10'] = {'type': 'grass', 'variant': 1, 'pos': (3 + i, 10)}  
+            self.tilemap[str(3 + i) + ';10'] = {'type': 'grass', 'variant': 1, 'pos': (3 + i, 10)}  # 3-10 on X, 10 on Y. Horizontal line of grass tiles.
+            self.tilemap['10;' + str(5 + i)] = {'type': 'stone', 'variant': 1, 'pos': (10, 5 + i)}  # 10 on X, 5-15 on Y. Vertical line of stone tiles. 
             
         
