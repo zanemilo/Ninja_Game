4e7a8b6dbff5477363cb28b5f31413c5fc0ae15b
Zane Deso <zanemilo@yahoo.com>
2024-07-24 14:40:22 -0400

    Animation class 2

--- scripts/utils.py
+++ scripts/utils.py
@@ -28,4 +28,18 @@ class Animation:
         self.done = False
         self.frame = 0
 
+    def copy(self):
+        return Animation(self.images, self.img_duration, loop=True)
+    
+    def update(self):
+        if self.loop:
+            self.frame = (self.frame + 1) % (self.img_duration * len(self.images))
+        else:
+            self.frame = min(self.frame + 1, self.img_duration * len(self.images) - 1)
+            if self.frame >= self.img_duration * len(self.images) - 1:
+                self.done = True
+    
+    def img(self):
+        return self.images[int(self.frame / self.img_duration)]
+
         
