a0e91391eae6a4fb7c88c60d7ca26dfb8bd4b0f8
Zane Deso <zanemilo@yahoo.com>
2024-07-24 12:43:47 -0400

    referenced tutorial files, found an error in parans around render pos method for Cloud. fixed. GAME IS: Functional ATM

--- scripts/clouds.py
+++ scripts/clouds.py
@@ -12,7 +12,7 @@ class Cloud:
     
     def render(self, surf, offset=(0, 0)):
         render_pos = (self.pos[0] - offset[0] * self.depth, self.pos[1] - offset[1] * self.depth)  # by multiplying by depth, we enable different speed of background scrolling in games. Parralax effect :)
-        surf.blit(self.img, (render_pos[0] % (surf.get_width() + self.img.get_width()) - self.img.get_width()), render_pos[1] % (surf.get_height() + self.img.get_height()) - self.img.get_height())
+        surf.blit(self.img, (render_pos[0] % (surf.get_width() + self.img.get_width()) - self.img.get_width(), render_pos[1] % (surf.get_height() + self.img.get_height()) - self.img.get_height()))
 
     
 class Clouds:
