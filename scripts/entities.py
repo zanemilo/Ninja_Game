1f0380573c116ae596224bfa536bf8001a883945
Zane Deso <zanemilo@yahoo.com>
2024-07-24 15:18:37 -0400

    fixed the bug, I forogt to add the index for anim_offset[0]. GAME IS: Functional ATM

--- scripts/entities.py
+++ scripts/entities.py
@@ -69,7 +69,7 @@ class PhysicsEntity:
 
     def render(self, surf, offset=(0, 0)):
         """Given surf param, blit the path to the current animation img at self.pos on the surf param with built in scroll offset and animating offset"""
-        surf.blit(pygame.transform.flip(self.animation.img(), self.flip, False), (self.pos[0] - offset[0] + self.anim_offset, self.pos[1] - offset[1] + self.anim_offset[1]))
+        surf.blit(pygame.transform.flip(self.animation.img(), self.flip, False), (self.pos[0] - offset[0] + self.anim_offset[0], self.pos[1] - offset[1] + self.anim_offset[1]))
 
 class Player(PhysicsEntity):
     def __init__(self, game, pos, size):
