ce83a64b1068c65c850a049ccde489cc1a7f6a59
Zane Deso <zanemilo@yahoo.com>
2024-08-05 15:28:34 -0400

    variant scroll implemented

--- editor.py
+++ editor.py
@@ -37,7 +37,8 @@ class Editor:
         self.tile_variant = 0  
 
         self.clicking = False
-        right_clicking = False
+        self.right_clicking = False
+        self.shift = False
 
     def run(self):
         while True:
@@ -58,10 +59,17 @@ class Editor:
                         self.clicking = True
                     if event.button == 3:
                         self.right_clicking = True
-                    if event.button == 4:
-                        self.tile_group = (self.tile_group - 1) % len(self.tile_list)  # loops over the list up to length then back to 0
-                    if event.button == 5:
-                        self.tile_group = (self.tile_group + 1) % len(self.tile_list)
+                    if self.shift:
+                        if event.button == 4:
+                            self.tile_variant = (self.tile_variant - 1) % len(self.assets[self.tile_list[self.tile_group]])  # loops over variants of current tile
+                        if event.button == 5:
+                            self.tile_variant = (self.tile_variant + 1) % len(self.assets[self.tile_list[self.tile_group]])
+                    else:
+                        if event.button == 4:
+                            self.tile_group = (self.tile_group - 1) % len(self.tile_list)  # loops over the list up to length then back to 0
+                        if event.button == 5:
+                            self.tile_group = (self.tile_group + 1) % len(self.tile_list)
+
 
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_LEFT:
@@ -72,6 +80,8 @@ class Editor:
                         self.movement[2] = True
                     if event.key == pygame.K_DOWN:
                         self.movement[3] = True
+                    if event.key == pygame.K_LSHIFT:
+                        self.shift = True
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_LEFT:
                         self.movement[0] = False
@@ -81,6 +91,8 @@ class Editor:
                         self.movement[2] = False
                     if event.key == pygame.K_DOWN:
                         self.movement[3] = False
+                    if event.key == pygame.K_LSHIFT:
+                        self.shift = False
 
             self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))  # Render display onto screen (window), transform the size of display to screen (get its size)
             pygame.display.update()
