22bb4988cfe543d49496724ce57ef2d1573f6056
Zane Deso <zanemilo@yahoo.com>
2024-08-06 11:31:55 -0400

    Implemented Tile placement preview in tile map editor

--- editor.py
+++ editor.py
@@ -55,6 +55,8 @@ class Editor:
             mpos = (mpos[0] / RENDER_SCALE, mpos[1] / RENDER_SCALE)
             tile_pos = (int((mpos[0] + self.scroll[0])  // self.tilemap.tile_size), int((mpos[1] + self.scroll[1]) // self.tilemap.tile_size))  # Provides coords of mouse in terms of tile system
 
+            self.display.blit(current_tile_img, (tile_pos[0] * self.tilemap.tile_size -  self.scroll[0], tile_pos[1] * self.tilemap.tile_size - self.scroll[1]))  # Scale to pixel coords
+
             if self.clicking:  # handles
                                     # converts index selection into str name for the group
                 self.tilemap.tilemap[str(tile_pos[0]) + ';' + str(tile_pos[1])] = {'type': self.tile_list[self.tile_group], 'variant': self.tile_variant, 'pos': tile_pos}
