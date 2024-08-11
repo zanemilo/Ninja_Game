999d7a45e7cbe89ab2c3132130e3b8a294a91588
Zane Deso <zanemilo@yahoo.com>
2024-08-10 20:20:11 -0400

    Implemented and integrated wall slide and wall jumping mechanic

--- scripts/entities.py
+++ scripts/entities.py
@@ -14,6 +14,8 @@ class PhysicsEntity:
         self.flip = False
         self.set_action('idle')
 
+        self.last_movement = [0, 0]
+
     def rect(self):
         """Reason for function is that we update our pos all the time and need a refreshed rect when required"""
         return pygame.Rect(self.pos[0], self.pos[1], self.size[0], self.size[1])
@@ -59,6 +61,8 @@ class PhysicsEntity:
             self.flip = False
         if movement[0] < 0:
             self.flip = True
+        
+        self.last_movement = movement
 
         self.velocity[1] = min(5, self.velocity[1] + 0.1)  # nice way of doing take the smaller number to make terminal velocity <= 5.
 
@@ -76,6 +80,7 @@ class Player(PhysicsEntity):
         super().__init__(game, 'player', pos, size)
         self.air_time = 0
         self.jumps = 1
+        self.walls_slide = False
          
     def update(self, tilemap, movement=(0, 0)):
         super().update(tilemap, movement=movement)
@@ -85,16 +90,47 @@ class Player(PhysicsEntity):
             self.air_time = 0
             self.jumps = 1
 
-        if self.air_time > 4:
-            self.set_action('jump')
-        elif movement[0] != 0:
-            self.set_action('run')
+        self.walls_slide = False  # Single frame switch to reset wall_slide
+        if (self.collisions['right'] or self.collisions['left']) and self.air_time > 4:  # if we hit the wall on either sied and we are in the air
+            self.walls_slide = True
+            self.velocity[1] = min(self.velocity[1], 0.5)  # Cap drop rate if wall sliding
+            if self.collisions['right']:
+                self.flip = False
+            else:
+                self.flip = True
+            self.set_action('wall_slide')
+
+        if not self.walls_slide:
+            if self.air_time > 4:
+                self.set_action('jump')
+            elif movement[0] != 0:
+                self.set_action('run')
+            else:
+                self.set_action('idle')
+
+        if self.velocity[0] > 0:
+            self.velocity[0] = max(self.velocity[0] - 0.1, 0)  # Pull velocity towards 0
         else:
-            self.set_action('idle')
+            self.velocity[0] = max(self.velocity[0] + 0.1, 0)
 
     def jump(self):
-        if self.jumps:
+        if self.walls_slide:
+            if self.flip and self.last_movement[0] < 0:
+                self.velocity[0] = 3.5  # push away from wall
+                self.velocity[1] = -2.5 # wall jump
+                self.air_time = 5
+                self.jumps = max(0, self.jumps - 1)
+                return True
+            elif not self.flip and self.last_movement[0] > 0:
+                self.velocity[0] = -3.5  # push away from wall
+                self.velocity[1] = -2.5 # wall jump
+                self.air_time = 5
+                self.jumps = max(0, self.jumps - 1)
+                return True
+
+        elif self.jumps:
             self.velocity[1] = -3
             self.jumps -= 1
             self.air_time = 5
+            return True
 
