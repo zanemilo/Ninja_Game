d487b53dcce8172c87d5e3f00fbde4a2f1839361
Zane Deso <zanemilo@yahoo.com>
2024-08-17 12:47:48 -0400

    implemented transitions circle between level transitions and death

--- main.py
+++ main.py
@@ -50,7 +50,9 @@ class Game:
         self.player = Player(self, pos=(50, 50), size=(8, 15))  # instantiate player obj for use in game/run function in main.
 
         self.tilemap = Tilemap(self, tile_size=16)  # self to pass in game reference to then instantiate Tilemap with default size 16
-        self.load_level(0)
+
+        self.level = 0
+        self.load_level(self.level)
 
         self.screenshake = 0
 
@@ -75,6 +77,7 @@ class Game:
 
         self.scroll = [0, 0]  # camera location
         self.dead = 0
+        self.transition = -30
 
     def run(self):
         while True:
@@ -82,10 +85,18 @@ class Game:
 
             self.screenshake = max(0, self.screenshake - 1)
 
+            if not len(self.enemies):
+                self.transition += 1
+                if self.transition > 30:
+                    self.level +=1
+                    self.load_level(self.level)
+            if self.transition < 0:
+                self.transition += 1
+
             if self.dead:  # once player is hit, this becomes true
                 self.dead += 1  # starts incrementing to 40
                 if self.dead > 40: # load level after 40
-                    self.load_level(0)
+                    self.load_level(self.level)
 
             # since orientation of camera is based on top left, subtract part of screen size to centerplayer then /30 will ramp slow/speed up depending on distance
             self.scroll[0] += (self.player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) /30
@@ -168,6 +179,12 @@ class Game:
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = False
 
+            if self.transition:
+                transition_surf = pygame.Surface(self.display.get_size())
+                pygame.draw.circle(transition_surf, (255, 255, 255), (self.display.get_width() // 2, self.display.get_height() // 2), (30 - abs(self.transition)) * 8)  # mult by 8 due to dimensions of screen
+                transition_surf.set_colorkey((255, 255, 255))
+                self.display.blit(transition_surf, (0, 0))
+                
             screenshake_offset = (random.random() * self.screenshake - self.screenshake / 2, random.random() * self.screenshake - self.screenshake / 2)
             self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), screenshake_offset)  # Render display onto screen (window), transform the size of display to screen (get its size)
             pygame.display.update()
